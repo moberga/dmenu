a15f17a670662d98fa1df14ea2396122c7110897
moberga <r.pezzotta1@campus.unimib.it>
2022-11-20 23:12:06 +0100

    changed theme to gruvbox

--- colors/gruvbox-dark.h
+++ colors/gruvbox-dark.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#000000", "#b16286" },
-	[SchemeSelHighlight] = { "#689d6a", "#000000" },
-	[SchemeNormHighlight] = { "#689d6a", "#000000" },
+	[SchemeSel] = { "#282828", "#ebdbb2" },
+	[SchemeSelHighlight] = { "#ebdbb2", "#504945" },
+	[SchemeNormHighlight] = { "#ebdbb2", "#504945" },
 	[SchemeOut] = { "#ffffff", "#458588" },
-	[SchemeMid] = { "#ebdbb2", "#1c1f24" },
+	[SchemeMid] = { "#ebdbb2", "#282828" },
 };
